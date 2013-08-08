dd84910fdf95f29cf15a8bf75ff23adf6798ac91
waigen <waigen@gmail.com>
2013-08-08 05:39:31 +0000

    Edited product desc in index.html

--- web.js
+++ web.js
@@ -1,17 +1,20 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html"; // new
 
 var app = express.createServer(express.logger());
 
-var fs = require('fs');
 
-var myStringBuf = fs.readFileSync('index.html');
+//var myStringBuf = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-  response.send(myStringBuf.toString());
+//  response.send(myStringBuf.toString());
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
